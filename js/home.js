fe74bd082670310d5ee005de631d3e2a38d9adb9
Marko <marko.vasquez@gmail.com>
2024-03-04 23:15:30 -0300

    firstCommit
    npm init
    created home.html
    created home.js

---
+++ js/home.js
@@ -0,0 +1,20 @@
+function calculateAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age
+}
+
+document.getElementById("testForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const getName = document.getElementById("name").value;
+    const getBdate = document.getElementById("bDate").value;
+
+    const age = calculateAge(getBdate);
+    alert(`Nombre: ${getName}, tu edad es: ${age}`);
+});
